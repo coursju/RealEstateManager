de666221b802f717b7293cca14497109839d4a7f
coursju <coursju@outlook.com>
2020-12-01 15:11:08 +0400

    migration to androidx

--- app/src/main/java/com/openclassrooms/realestatemanager/SecondActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/SecondActivity.java
@@ -1,7 +1,8 @@
 package com.openclassrooms.realestatemanager;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
